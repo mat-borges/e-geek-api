f7b3287bf3e8ad9f55ab7a3d31bc240422149a2c
Mateus Borges <borgesm.work@gmail.com>
2022-11-23 13:11:30 -0300

    feat!: add route to create products on mongoDB (need to remove it later)

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import express, { json } from 'express';
 
 import cors from 'cors';
 import dotenv from 'dotenv';
+import manageProductRouter from './routes/manageProductsCollection.js';
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(json());
+
+app.use(manageProductRouter);
 
 app.listen(process.env.PORT, () => console.log(`Running server on http://localhost:${process.env.PORT}`));
---
+++ src/routes/manageProductsCollection.js
@@ -0,0 +1,42 @@
+import Joi from 'joi';
+import { Router } from 'express';
+import { productsCollection } from '../db/db.js';
+
+const router = Router();
+
+const productSchema = Joi.object({
+	name: Joi.string().required().label('Name'),
+	price: Joi.number().precision(2).required().label('Price'),
+	tags: Joi.array().items(Joi.string().required()).label('tags'),
+	mainimage: Joi.string().uri().required().label('MainImage'),
+	images: Joi.array().items(Joi.string().uri()).label('Images'),
+	description: Joi.string().required().label('Description'),
+	sizes: Joi.array().items(Joi.string().min(1)).label('Sizes'),
+	purchases: Joi.number().integer().required().label('Purchases'),
+});
+
+router.post('/editProduct', async (req, res) => {
+	const productData = req.body;
+	const { error, value: product } = productSchema.validate(productData, { abortEarly: false });
+	if (error) {
+		const errors = error.details.map((detail) => detail.message);
+		return res.status(422).send({ message: errors });
+	}
+	try {
+		await productsCollection.insertOne(product);
+		res.status(200).send(product);
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
+	}
+});
+
+router.put('/editProduct', (req, res) => {
+	res.status(404).send('Not implemented yet');
+});
+
+router.delete('/editProduct', (req, res) => {
+	res.status(404).send('Not implemented yet');
+});
+
+export default router;
