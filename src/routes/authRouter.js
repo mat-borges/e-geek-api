97a5995014775a2f02a2b9eac5aedf902f9f4b6e
Mateus Borges <borgesm.work@gmail.com>
2022-11-23 15:18:04 -0300

    feat: sign up validation

---
+++ src/routes/authRouter.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import { postSignUp } from '../controllers/authController.js';
+import { validateSignUp } from '../middlewares/singUpValidationMiddleware.js';
+
+const router = Router();
+
+router.post('/sign-up', validateSignUp, postSignUp);
