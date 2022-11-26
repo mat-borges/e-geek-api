27065c0e1bc844887734da87d29a5f493b1935d8
Lucas Macedo <lucasamacedo@hotmail.com>
2022-11-26 14:47:07 -0300

    feat!: create connect without sign-in

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,51 +1,76 @@
-import { usersCollection, sessionsCollection } from '../db/db.js';
-import { v4 as uuid } from 'uuid';
+import { usersCollection, sessionsCollection } from "../db/db.js";
+import { v4 as uuid } from "uuid";
 
 export async function postSignUp(req, res) {
 	const { user } = res.locals;
 
 	try {
 		await usersCollection.insertOne(user);
-		res.status(200).send({ message: 'User registered successfully' });
+		res.status(200).send({ message: "User registered successfully" });
 	} catch (err) {
 		console.log(err);
 		res.sendStatus(500);
 	}
 }
 
-export async function postSignIn(req, res){
+export async function postSignIn(req, res) {
 	const { user } = res.locals;
 
-	try{
+	try {
 		const token = uuid();
-		const userData = await usersCollection.findOne({email: user.email});
-		const sessionExists = await sessionsCollection.findOne({userId: userData._id});
+		const userData = await usersCollection.findOne({ email: user.email });
+		const sessionExists = await sessionsCollection.findOne({
+			userId: userData._id,
+		});
 		const body = {
 			token: token,
-			name: user.name
+			name: user.name,
 		};
 
-		if(sessionExists){
-			await sessionsCollection.updateOne({
-				userId: userExists._id
-			},{
-				$set:{
-					lastStatus: Date.now(),
-					token
+		if (sessionExists) {
+			await sessionsCollection.updateOne(
+				{
+					userId: userExists._id,
+				},
+				{
+					$set: {
+						lastStatus: Date.now(),
+						token,
+					},
 				}
-			});
-		}else{
+			);
+		} else {
 			await sessionsCollection.insertOne({
 				userId: userExists._id,
 				lastStatus: Date.now(),
-				token
+				token,
 			});
 		}
 
-		res.send(body)
+		res.send(body);
 	} catch (err) {
 		console.log(err);
 		res.sendStatus(500);
 		return;
+	}
+}
+
+export async function getNewSession(req, res) {
+	const token = uuid();
+
+	try {
+		await sessionsCollection.insertOne({
+			userId: null,
+			token,
+			lastStatus: Date.now(),
+		});
+		const body = {
+			isLogged: false,
+			token,
+		};
+		res.send(body);
+	} catch (err) {
+		console.log(err);
+		res.send(500);
 	}
 }
--- src/models/signInSchema.js
+++ src/models/signInSchema.js
@@ -1,11 +1,11 @@
-import joi from 'joi';
+import joi from "joi";
 
 export const signInSchema = joi.object({
-	email: joi.string().email().required().label('E-mail'),
+	email: joi.string().email().required().label("E-mail"),
 	password: joi
 		.string()
 		.min(8)
 		.pattern(/^([a-zA-Z0-9@*#!.,$%]{8,})$/)
 		.required()
-		.label('Password'),
+		.label("Password"),
 });
